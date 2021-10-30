ebdc2fe2a86ba5994f81d7e52fcd642396a7b500
LuisBall96 <luisballesteros96@hotmail.com>
2021-10-30 11:55:35 -0500

    nada

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, flash, redirect
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager, UserMixin, login_user, login_required, current_use
+from flask_login import LoginManager, UserMixin, login_user, login_required, current_user
 import controlador
 
 
